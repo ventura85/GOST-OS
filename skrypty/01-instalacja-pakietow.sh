1ddcf784891852a77ac4f643f68f74bfb1b15439
Kamil Lewandowski <ventura85@o2.pl>
2025-09-16 11:39:38 +0200

    Finalizacja Fazy 1: Pena personalizacja wygldu i GRUB-a

--- skrypty/01-instalacja-pakietow.sh
+++ skrypty/01-instalacja-pakietow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Wersja 1.3 - Skrypt instalujący pakiety dla pulpitu i ekranu logowania
+# Wersja 1.4 - Skrypt instalujący pakiety dla pulpitu i ekranu logowania
 
 echo ">>> Instalowanie podstawowych narzędzi..."
 sudo apt install -y git sudo
 
-echo ">>> Instalowanie środowiska graficznego XFCE..."
-sudo apt install -y xfce4 xfce4-goodies
+echo ">>> Instalowanie środowiska graficznego XFCE i niezbędnych narzędzi..."
+sudo apt install -y xfce4 xfce4-goodies menulibre mugshot
 
 echo ">>> Instalowanie zależności dla motywów i dodatków..."
 sudo apt install -y librsvg2-bin gtk2-engines-murrine gtk2-engines-pixbuf
--- skrypty/03-kopiowanie-konfiguracji.sh
+++ skrypty/03-kopiowanie-konfiguracji.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# Wersja 2.0 - Skrypt kopiujący finalne pliki konfiguracyjne i graficzne
+# Wersja 7.0 - Skrypt kopiujący finalne pliki konfiguracyjne i graficzne
 
 echo ">>> Czyszczenie domyślnych tapet Debiana..."
 sudo rm -f /usr/share/images/desktop-base/desktop*
 
 echo ">>> Kopiowanie grafiki systemowej..."
-# Zakładamy, że ten skrypt jest uruchamiany z głównego folderu projektu
+# Tworzymy dedykowany folder na naszą grafikę, żeby był porządek
+sudo mkdir -p /usr/share/backgrounds/gostos
+
+# Kopiujemy wszystkie potrzebne pliki graficzne w odpowiednie, logiczne miejsca
 sudo cp zasoby/branding/wallpaper_gostos.jpg /usr/share/images/desktop-base/
 sudo cp zasoby/branding/login-background.png /usr/share/backgrounds/gostos/
+sudo cp zasoby/branding/avatar.png /usr/share/backgrounds/gostos/
 sudo cp zasoby/branding/gostos-start.png /usr/share/pixmaps/
-
-# Podmiana domyślnego awatara systemowego (metoda "hakerska")
-AVATAR_PATH="/usr/share/icons/Adwaita/scalable/status/avatar-default.svg"
-if [ -f "$AVATAR_PATH" ]; then
-    sudo mv "$AVATAR_PATH" "$AVATAR_PATH.kopia"
-fi
-sudo cp zasoby/branding/avatar.png "$AVATAR_PATH"
 
 echo ">>> Kopiowanie plików konfiguracyjnych..."
 # Używamy zmiennej $USER, aby skrypt działał dla każdego użytkownika
@@ -27,12 +24,19 @@ mkdir -p $CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml
 cp -r konfiguracja/pulpit/* $CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml/
 cp -r konfiguracja/panel/* $CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml/
 
+# Kopiowanie niestandardowej struktury menu (z menulibre)
+echo ">>> Kopiowanie niestandardowej struktury menu..."
+mkdir -p /home/$USER/.config/menus
+mkdir -p /home/$USER/.local/share/applications
+cp konfiguracja/menu/xfce-applications.menu /home/$USER/.config/menus/
+cp -r konfiguracja/menu/custom-applications/* /home/$USER/.local/share/applications/
+
 # Własne style CSS i GTK2
 mkdir -p $CONFIG_DIR/gtk-3.0
 cp konfiguracja/motywy/gtk.css $CONFIG_DIR/gtk-3.0/
 cp konfiguracja/motywy/.gtkrc-2.0 /home/$USER/
 
-# Konfiguracja LightDM
+# Konfiguracja LightDM (ekran logowania)
 sudo cp konfiguracja/logowanie/lightdm-gtk-greeter.conf /etc/lightdm/
 
 # Aktywatory pulpitu
@@ -40,6 +44,45 @@ mkdir -p $DESKTOP_DIR
 cp -r konfiguracja/pulpit/skroty/* $DESKTOP_DIR
 echo ">>> Ufanie aktywatorom pulpitu..."
 chmod +x $DESKTOP_DIR/*.desktop
+
+# Ustawianie awatara użytkownika dla Menu Whisker
+echo ">>> Ustawianie awatara użytkownika w menu..."
+cp /usr/share/backgrounds/gostos/avatar.png /home/$USER/.face
+# Upewniamy się, że plik należy do użytkownika, co jest kluczowe
+sudo chown $USER:$USER /home/$USER/.face
+
+# --------------------------------------------------------------------
+# SEKCJA KONFIGURACJI SYSTEMU I GRUB-A
+# --------------------------------------------------------------------
+
+# Zmiana nazwy dystrybucji
+echo ">>> Zmienianie nazwy systemu na GOST OS..."
+sudo cp konfiguracja/system/os-release /etc/os-release
+
+# Konfiguracja GRUB (tło i język)
+echo ">>> Konfigurowanie GRUB..."
+
+# Krok 1: Kopiujemy nasze niestandardowe tło (w wersji 256 kolorów)
+sudo cp zasoby/branding/gostos-grub-background.png /boot/grub/
+
+# Krok 2: Modyfikujemy plik /etc/default/grub, aby używał prostego tła
+# Usuwamy stare wpisy (jeśli istnieją), aby uniknąć konfliktów
+sudo sed -i '/GRUB_THEME/d' /etc/default/grub
+sudo sed -i '/GRUB_BACKGROUND/d' /etc/default/grub
+sudo sed -i '/GRUB_LANG/d' /etc/default/grub
+sudo sed -i '/GRUB_TERMINAL_OUTPUT/d' /etc/default/grub
+
+# Dodajemy poprawne ustawienia na końcu pliku
+echo '' | sudo tee -a /etc/default/grub
+echo '# --- Konfiguracja dla GOST OS ---' | sudo tee -a /etc/default/grub
+echo 'GRUB_BACKGROUND="/boot/grub/gostos-grub-background.png"' | sudo tee -a /etc/default/grub
+echo 'GRUB_LANG="pl_PL"' | sudo tee -a /etc/default/grub
+echo 'GRUB_TERMINAL_OUTPUT="gfxterm"' | sudo tee -a /etc/default/grub
+echo '# ------------------------------' | sudo tee -a /etc/default/grub
+
+# Krok 3: Aktualizujemy konfigurację GRUB-a, aby zastosować wszystkie zmiany
+echo ">>> Aktualizowanie konfiguracji GRUB..."
+sudo update-grub
 
 echo ">>> Finalna konfiguracja wyglądu została zastosowana."
 echo ">>> Uruchom ponownie komputer, aby zobaczyć wszystkie zmiany."
